67793a7c4e56810c8a08d5c83384baee0aefafa7
Jose89fcb <60312427+jose89fcb@users.noreply.github.com>
2022-11-05 18:38:37 +0100

    Delete Noticias Fansites.py

--- Noticias Fansites.py
+++
@@ -1,7 +0,0 @@
-import requests
-import html
-url =  requests.get(f"https://images.habbo.com/habbo-web-news/es/production/front.json")
-
-NoticiasFansites =  html.unescape(url.json()[1]['summary']) #Noticias Fansites
-
-print("Noticias Fansites: " + NoticiasFansites)
