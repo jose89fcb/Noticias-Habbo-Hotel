dfa0da5880b83f3133573905a053eb35f7ed9410
Jose89fcb <60312427+jose89fcb@users.noreply.github.com>
2022-11-05 18:39:22 +0100

    Update Actualizaciones Habbo Hotel.py

--- Actualizaciones Habbo Hotel.py
+++ Actualizaciones Habbo Hotel.py
@@ -1,5 +1,5 @@
 import requests
 import html
 url =  requests.get(f"https://images.habbo.com/habbo-web-news/es/production/front.json")
-ActualizacionesHabboHotel =  html.unescape(url.json()[2]['summary']) #Actualizaciones Habbo Hotel
+ActualizacionesHabboHotel =  html.unescape(url.json()[1]['summary']) #Actualizaciones Habbo Hotel
 print("Actualizaciones HabboHotel: " + ActualizacionesHabboHotel)
