da1c0cd3e004bfc15f90e0938cec06ac803f8a1d
Jose89fcb <60312427+jose89fcb@users.noreply.github.com>
2022-11-05 18:38:44 +0100

    Delete Seguridad Habbo Hotel.py

--- Seguridad Habbo Hotel.py
+++
@@ -1,5 +0,0 @@
-import requests
-import html
-url =  requests.get(f"https://images.habbo.com/habbo-web-news/es/production/front.json")
-SeguridadHabbo = html.unescape(url.json()[10]['summary']) #Seguridad Habbo Hotel
-print("Seguridad Habbo: " +SeguridadHabbo)
