3aeadf8a6299ab47a5fdd8a4d156b42b085ae595
Jose89fcb <60312427+jose89fcb@users.noreply.github.com>
2022-11-05 18:38:29 +0100

    Delete Builders At Work.py

--- Builders At Work.py
+++
@@ -1,5 +0,0 @@
-import requests
-import html
-url =  requests.get(f"https://images.habbo.com/habbo-web-news/es/production/front.json")
-BuildersAtWork = html.unescape(url.json()[4]['summary']) #Constructores
-print("Builders At Work: " + BuildersAtWork)
